2d760cb8e6b85203ef4ecb05fb1cefad252fba14
Zhang Tao <191094610@qq.com>
2020-08-11 10:06:56 +0800

    my-react-redux-提交-01

--- src/index.js
+++ src/index.js
@@ -1,17 +1,7 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
-
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
-
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+// 引入React核心库
+import React from 'react'
+// 引入ReactDOM库
+import ReactDOM from 'react-dom'
+// 引入App组件
+import App from './App'
+ReactDOM.render(<App/>,document.getElementById('root'))
